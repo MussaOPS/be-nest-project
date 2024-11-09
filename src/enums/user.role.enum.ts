5f5db58dad19b23281d3769712656da639c7225c
MussaOPS1 <musa011999@gmail.com>
2024-11-09 13:40:44 +0500

    init

---
+++ src/enums/user.role.enum.ts
@@ -0,0 +1,5 @@
+export enum UsersRole {
+    ADMIN = 'ADMIN',
+    USER = 'USER',
+    SELLER = 'SELLER'
+}
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,31 +1,60 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
-import { ApiProperty } from "@nestjs/swagger";
+import {Entity, Column, PrimaryGeneratedColumn} from "typeorm";
+import {ApiProperty} from "@nestjs/swagger";
+import {UsersRole} from "../enums/user.role.enum";
 
 @Entity()
 export class User {
-  @ApiProperty({
-    example: 1,
-    description: "Уникальный идентификатор пользователя",
-  })
-  @PrimaryGeneratedColumn()
-  id: number;
+    @ApiProperty({
+        example: 1,
+        description: "Уникальный идентификатор пользователя",
+    })
+    @PrimaryGeneratedColumn()
+    id: string;
 
-  @ApiProperty({ example: "john_doe", description: "Имя пользователя" })
-  @Column({ unique: true })
-  username: string;
+    @ApiProperty({example: "john_doe", description: "Имя пользователя"})
+    @Column({unique: true})
+    username: string;
 
-  @ApiProperty({
-    example: "john@example.com",
-    description: "Email пользователя",
-  })
-  @Column({ unique: true })
-  email: string;
+    @ApiProperty({
+        example: "john@example.com",
+        description: "Email пользователя",
+    })
+    @Column({unique: true})
+    email: string;
 
-  @ApiProperty({ example: "password123", description: "Пароль пользователя" })
-  @Column()
-  password: string;
+    @ApiProperty({example: "password123", description: "Пароль пользователя"})
+    @Column()
+    password: string;
 
-  @ApiProperty({ example: "John Doe", description: "Полное имя пользователя" })
-  @Column()
-  fullName: string;
+    @ApiProperty({example: "John Doe", description: "Имя пользователя"})
+    @Column()
+    firstName: string;
+
+    @ApiProperty({example: "Doe", description: "Фамилия пользователя"})
+    @Column()
+    lastName: string;
+
+    @ApiProperty({example: "+77007272727", description: "Номер телефона пользователя"})
+    @Column()
+    phone: string;
+
+    @ApiProperty({example: "test.png", description: "Аватарка пользователя"})
+    @Column()
+    avatar: string;
+
+    @ApiProperty({example: "Сыганак 10", description: "Адрес пользователя"})
+    @Column()
+    address: string;
+
+    @ApiProperty({example: "ADMIN", description: "Роль пользователя"})
+    @Column()
+    role: UsersRole;
+
+    @ApiProperty({example: "2021-06-01T00:00:00.000Z", description: "Дата создания пользователя"})
+    @Column()
+    created: Date;
+
+    @ApiProperty({example: "2021-06-01T00:00:00.000Z", description: "Дата обновления пользователя"})
+    @Column()
+    updated: Date;
 }

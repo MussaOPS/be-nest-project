681c990dea24d255de42927bafcd526ad91f8ab1
MussaOPS1 <musa011999@gmail.com>
2024-12-14 15:00:56 +0500

    handler + and etc

--- src/service/impl/default-users.service.ts
+++ src/service/impl/default-users.service.ts
@@ -1,12 +1,13 @@
 import {Injectable, NotFoundException, UnauthorizedException} from '@nestjs/common';
-import {DataSource, Repository} from 'typeorm';
+import {DataSource, QueryFailedError, Repository} from 'typeorm';
 import {InjectRepository} from '@nestjs/typeorm';
 import {JwtService} from '@nestjs/jwt';
 import {UsersService} from "../users.interface";
 import {User} from "../../entity/user.entity";
 import {PasswordUtil} from "../../util/password.util";
-import {JsonUtil} from "../../util/json.util";
 import {JwtResponseDto} from "../../dto/jwt-response.dto";
+import {CommonBusinessException} from "../../errors/exception/common.business-exception";
+import {CommonValidationException} from "../../errors/exception/common.validation-exception";
 
 @Injectable()
 export class DefaultUsersService implements UsersService {
@@ -21,30 +22,25 @@ export class DefaultUsersService implements UsersService {
 
     async signUp(user: User, password: string): Promise<User> {
 
-        return await this.dataSource.transaction(async (manager) => {
-            const userAdditionalInfo = new UserAdditionalInfoModel(createUserDto.avatar, createUserDto.address);
-            const userAdditionalInfoJsonString = JsonUtil.stringify<UserAdditionalInfoModel>(userAdditionalInfo);
+        try {
+            user.password = await PasswordUtil.hashPassword(password);
 
-            const userAdditionalInfoEntity = manager.create(UserAdditionalInfoEntity, {
-                data: userAdditionalInfoJsonString,
-            });
-            const savedUserAdditionalInfoEntity = await manager.save<UserAdditionalInfoEntity>(userAdditionalInfoEntity);
+            return this.usersRepository.create(user);
+        } catch (e) {
+            if (e instanceof QueryFailedError) {
+                const errorMessage = e.driverError?.message || '';
 
-            const hashedPassword = await PasswordUtil.hashPassword(createUserDto.password);
+                if (errorMessage.includes('null value in column')) {
+                    throw new CommonValidationException('VALIDATION_ERROR','Required fields are missing');
+                }
+                if (errorMessage.includes('duplicate key value violates unique constraint')) {
+                    throw new CommonBusinessException('USER_ALREADY_EXITS','User already exists');
+                }
+            }
 
-            const user = manager.create(User, {
-                username: createUserDto.username,
-                email: createUserDto.email,
-                firstName: createUserDto.firstName,
-                lastName: createUserDto.lastName,
-                phone: createUserDto.phone,
-                role: createUserDto.role,
-                userAdditionalInfoId: savedUserAdditionalInfoEntity.id,
-                password: hashedPassword,
-            });
-
-            return manager.save(User, user);
-        });
+            // Неизвестные ошибки
+            throw new Error('An unexpected error occurred');
+        }
     }
 
     async signIn(login: string, password: string): Promise<JwtResponseDto> {
